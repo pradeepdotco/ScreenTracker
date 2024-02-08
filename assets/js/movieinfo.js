3df2a2edd8e21bd4d24d8c88efc5cad7c8caa81e
destro <irtizasafdar@hotmail.co.uk>
2024-02-08 03:37:43 +0000

    edited movie plot section

--- assets/js/movieinfo.js
+++ assets/js/movieinfo.js
@@ -112,9 +112,7 @@ function renderSavedMovie() {
       <div class='card-body'>
         <h5 class='card-title'>${movie.Title}</h5>
         <p class='card-text'>
-          This is a wider card with supporting text below as a natural
-          lead-in to additional content. This content is a little bit
-          longer.
+          ${movie.Plot || 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'}
         </p>
         <p>TMDB Rating: ${movie.Ratings && movie.Ratings[0] ? movie.Ratings[0].Value : 'N/A'}</p>
         <button class='btn btn-primary' onclick="removeMovie('${movie.imdbID}')">Remove</button>
