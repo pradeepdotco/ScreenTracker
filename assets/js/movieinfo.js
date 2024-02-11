79e746f1c7d5943d07c6941f686ccaa6e0de639c
destro <irtizasafdar@hotmail.co.uk>
2024-02-11 16:36:09 +0000

    added youtube api for demo of app, also added trailers api with a search filter

--- assets/js/movieinfo.js
+++ assets/js/movieinfo.js
@@ -226,32 +226,30 @@ function toggleOverview(cardId) {
 
 // window.onload = renderMovieCards;
 
-async function fetchMissingImages() {
-  const apiKey = 'c765a205274e5ae2e4ea7651076c9be2';
-  
-  for (const movie of savedmovies) {
-    // Check if the movie has a poster path and if the image has been displayed
-    if (movie.Poster !== 'N/A' && !movie.imageDisplayed) {
-      try {
-        // Fetch the image from TMDB
-        // const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${movie.Title}&page=1&include_adult=false`);
-        const response = await fetch(`https://image.tmdb.org/t/p/w500${movie.Poster}`)
-        if (response.ok) {
-          const imageUrl = `https://image.tmdb.org/t/p/w500${movie.Poster}`;
-          // const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query${movie.Title}&page=1&include_adult=false`
-          console.log(movieUrl)
-          // Set the image URL in your movie object or handle it as needed
-          // movie.imageUrl = imageUrl;
-          movie.imageDisplayed = true; // Mark the image as displayed
-          console.log(`Fetched image for ${movie.Title}`);
-          // Render the updated watch library after fetching the image
-          renderSavedMovie();
-        } else {
-          console.error(`Failed to fetch image for ${movie.Title}`);
-        }
-      } catch (error) {
-        console.error(`Error fetching image for ${movie.Title}: ${error.message}`);
-      }
-    }
+// trailer code 
+function searchTrailer() {
+  const apiKey = 'AIzaSyBTkJPldmgGn0_lxLIhaQ0kqshfmKztDG0'; // YouTube API key
+  const filmTitle = document.getElementById('trailerInput').value;
+  if (!filmTitle) {
+   alert('Please enter a movie title.');
+   return;
   }
+  const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${filmTitle} trailer&type=video&key=${apiKey}`;
+  fetch(apiUrl)
+   .then(response => response.json())
+   .then(data => {
+    if (data.items.length > 0) {
+     const firstVideoId = data.items[0].id.videoId;
+     const videoContainer = document.getElementById('videoContainer');
+     // Embedding the YouTube video player or perform any other action
+     videoContainer.innerHTML = `
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/${firstVideoId}" frameborder="0" allowfullscreen></iframe>
+     `;
+    } else {
+     alert('Trailer not found.');
+    }
+   })
+   .catch(error => console.error('API call failed:', error));
 }
+
+ document.cookie = "cookieName=cookieValue; SameSite=None; Secure";
